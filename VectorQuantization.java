449bd25bdaf00d5c4cf901157dc5b65919ac5dd2
Yusuf Badr <yusufbadr@yahoo.com>
2023-12-04 22:37:23 +0200

    Binary file output

--- VectorQuantization.java
+++ VectorQuantization.java
@@ -3,8 +3,9 @@ import org.apache.commons.math3.linear.RealMatrix;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.Map;
+
 
 public class VectorQuantization {
     String inputImageName;
@@ -55,6 +56,35 @@ public class VectorQuantization {
 
             // Save the compressed image
             ImageIO.write(compressedImage, "jpg", new File(outputImagePath));
+
+
+
+            // writing to a binary file
+            try(ObjectOutputStream outputStream = new ObjectOutputStream((new FileOutputStream("output.dat")))){
+                outputStream.writeObject(compressedImage);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
+
+//            // reading from a binary file
+//
+//            try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("output.dat"))) {
+//                Object object = inputStream.readObject();
+//
+//                if (object instanceof BufferedImage) {
+//                    BufferedImage compressedImageFromBinary = (BufferedImage) object;
+//                    // Use the BufferedImage as needed
+//                } else {
+//                    // Handle the case where the object is not a BufferedImage
+//                    System.err.println("Unexpected object type in the file");
+//                }
+//
+//            } catch (IOException | ClassNotFoundException e) {
+//                e.printStackTrace();
+//            }
+
+
 
             System.out.println("Image compression completed.");
 
